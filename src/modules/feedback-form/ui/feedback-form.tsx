d46651b9fc784d1c1bab7c14babdf4066ea46cd0
muslik <ahmetaliev345@gmail.com>
2025-06-06 12:05:46 +0500

    fix

---
+++ src/modules/feedback-form/ui/feedback-form.tsx
@@ -0,0 +1,14 @@
+import { useForm } from "react-hook-form"
+
+
+
+export const FeedbackForm = () => {
+    const {} = useForm()
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
